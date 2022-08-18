1790a2e8d44f858179ceee317397b13b803cb7b5
sabbirz <sabbir.kuasha@gmail.com>
2022-08-18 18:30:58 +0600

    NID WIP

---
+++ src/lib/Card.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let title = "Upload Front Side NID"
+    export let img = "https://placeimg.com/400/225/arch"
+    export let buttonText = "Upload Front Side of your NID"
+</script>
+
+<div class="card w-96 bg-slate-900 shadow-xl ">
+    <figure><img src={img} alt="Nid" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">{title}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">{buttonText}</button>
+      </div>
+    </div>
+</div>
--- src/routes/checkNID/+page.svelte
+++ src/routes/checkNID/+page.svelte
@@ -1,5 +1,34 @@
-<main>
-    <div class="text-center p-10">
-        NID Check
+<script>
+    import Card from "$lib/Card.svelte";
+</script>
+
+<main class="w-screen p-5">
+    <div>
+        <h1 class="text-2xl pb-5 text-center"> Upload front and back side of your NID</h1>
     </div>
+    <div class="flex flex-col w-full lg:flex-row justify-center">
+        <div class="w-auto lg:w-2/6 flex flex-col justify-center justify-items-center">
+            <h1 class="rounded text-center p-2 ring-2">Front NID</h1>
+            <div class="mt-2 w-full flex justify-center">
+                <Card />
+            </div>
+            
+        </div>
+        
+        <div class="divider lg:divider-horizontal px-4">
+            <span class="text-2xl">&</span> 
+        </div> 
+
+        <div class="w-auto lg:w-2/6 flex flex-col justify-center">
+            <h1 class="rounded text-center p-2 ring-2">Back NID</h1>
+            <div class="mt-2 w-full flex justify-center">
+                <Card   title="Upload Back Side NID"
+                    img = "https://placeimg.com/400/225/paper"
+                    buttonText = "Upload Back Side of your NID"/>
+            </div>
+            
+        </div>
+        
+    </div>
+
 </main>
